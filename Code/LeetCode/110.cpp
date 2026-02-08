12ecf87a22cb4f0e06a65c83c7e3f046c1a480cd
bianwoyali-design <bianwoyali@gmail.com>
2026-02-08 13:34:58 +0800

    Add files via upload

---
+++ Code/LeetCode/110.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    int judge(TreeNode *node)
+    {
+        if (node == nullptr)
+            return 0;
+
+        int left = judge(node->left);
+        int right = judge(node->right);
+        if (left == -1 || right == -1 || abs(left - right) > 1)
+            return -1;
+        else
+            return max(left, right) + 1;
+    }
+
+    bool isBalanced(TreeNode *root)
+    {
+        return judge(root) >= 0;
+    }
+};
---
+++ Code/LeetCode/94.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    vector<int> inorderTraversal(TreeNode *root)
+    {
+        vector<int> res;
+
+        auto dfs = [&](this auto &&dfs, TreeNode *node) -> void
+        {
+            if (node == nullptr)
+                return;
+            dfs(node->left);
+            res.push_back(node->val);
+            dfs(node->right);
+        };
+
+        dfs(root);
+
+        return res;
+    }
+};
