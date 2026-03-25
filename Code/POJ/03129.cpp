38f8d28e3b9520572f225fbe842f6ee15010367e
bianwoyali-design <bianwoyali@gmail.com>
2026-03-25 20:55:27 +0800

    modified:   Code/POJ/03129.cpp

--- Code/POJ/03129.cpp
+++ Code/POJ/03129.cpp
@@ -2,6 +2,7 @@
 #include <cstdint>
 #include <iomanip>
 #include <iostream>
+#include <memory>
 #include <string_view>
 
 enum class WarriorType : std::uint8_t {
@@ -28,24 +29,29 @@ constexpr std::array<WarriorType, 5> BLUE_ORDER = {
 
 class Headquarter {
 public:
-  Headquarter(std::string_view name, int initial_m,
-              const std::array<int, WARRIOR_COUNT> &hp_costs,
-              const std::array<WarriorType, 5> &build_order)
-      : m_name(name), m_total_m(initial_m), m_costs(hp_costs),
-        m_order(build_order) {}
+  Headquarter(int initial_m, const std::array<int, WARRIOR_COUNT> &hp_costs)
+      : m_total_m(initial_m), m_costs(hp_costs) {}
+
+  virtual ~Headquarter() = default;
+
+  [[nodiscard]] virtual auto get_name() const noexcept -> std::string_view = 0;
+  [[nodiscard]] virtual auto
+  get_order() const noexcept -> const std::array<WarriorType, 5> & = 0;
 
   void step(int time) {
     if (m_stopped)
       return;
 
+    const auto &order = get_order();
+
     for (int i = 0; i < 5; ++i) {
-      WarriorType type = m_order[m_iter];
+      WarriorType type = order[m_iter];
       int cost = m_costs[static_cast<int>(type)];
 
       if (m_total_m >= cost) {
         m_total_m -= cost;
-        m_counts[static_cast<int>(type)]++;
-        m_total_count++;
+        ++m_counts[static_cast<int>(type)];
+        ++m_total_count;
 
         log_production(time, type, cost);
 
@@ -62,17 +68,16 @@ public:
 
   [[nodiscard]] auto is_stopped() const noexcept -> bool { return m_stopped; }
 
-private:
-  std::string_view m_name;
+protected:
   int m_total_m;
   const std::array<int, WARRIOR_COUNT> &m_costs;
-  const std::array<WarriorType, 5> &m_order;
 
   std::array<int, WARRIOR_COUNT> m_counts{0};
   int m_total_count{0};
   int m_iter{0};
   bool m_stopped{false};
 
+private:
   void print_time(int time) const {
     std::cout << std::setfill('0') << std::setw(3) << time << ' ';
   }
@@ -80,15 +85,41 @@ private:
   void log_production(int time, WarriorType type, int cost) const {
     print_time(time);
     auto type_idx = static_cast<int>(type);
-    std::cout << m_name << ' ' << WARRIOR_NAMES[type_idx] << ' '
+    std::cout << get_name() << ' ' << WARRIOR_NAMES[type_idx] << ' '
               << m_total_count << " born with strength " << cost << ','
               << m_counts[type_idx] << ' ' << WARRIOR_NAMES[type_idx] << " in "
-              << m_name << " headquarter\n";
+              << get_name() << " headquarter\n";
   }
 
   void log_stop(int time) const {
     print_time(time);
-    std::cout << m_name << " headquarter stops making warriors\n";
+    std::cout << get_name() << " headquarter stops making warriors\n";
+  }
+};
+
+class RedHeadquarter : public Headquarter {
+public:
+  using Headquarter::Headquarter;
+
+  [[nodiscard]] auto get_name() const noexcept -> std::string_view override {
+    return "red";
+  }
+  [[nodiscard]] auto
+  get_order() const noexcept -> const std::array<WarriorType, 5> & override {
+    return RED_ORDER;
+  }
+};
+
+class BlueHeadquarter : public Headquarter {
+public:
+  using Headquarter::Headquarter;
+
+  [[nodiscard]] auto get_name() const noexcept -> std::string_view override {
+    return "blue";
+  }
+  [[nodiscard]] auto
+  get_order() const noexcept -> const std::array<WarriorType, 5> & override {
+    return BLUE_ORDER;
   }
 };
 
@@ -109,13 +140,15 @@ auto main() -> int {
 
     std::cout << "Case:" << caseIdx << '\n';
 
-    Headquarter red("red", M, warrior_HP, RED_ORDER);
-    Headquarter blue("blue", M, warrior_HP, BLUE_ORDER);
+    std::array<std::unique_ptr<Headquarter>, 2> bases = {
+        std::make_unique<RedHeadquarter>(M, warrior_HP),
+        std::make_unique<BlueHeadquarter>(M, warrior_HP)};
 
     int time = 0;
-    while (!red.is_stopped() || !blue.is_stopped()) {
-      red.step(time);
-      blue.step(time);
+    while (!bases[0]->is_stopped() || !bases[1]->is_stopped()) {
+      for (auto &base : bases) {
+        base->step(time);
+      }
       time++;
     }
   }
