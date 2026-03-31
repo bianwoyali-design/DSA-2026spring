c122d216fba516fdc88b392afa840763885cb4ca
bianwoyali-design <bianwoyali@gmail.com>
2026-03-31 17:04:48 +0800

    modified:   Code/LeetCode/35.cpp

--- Code/LeetCode/35.cpp
+++ Code/LeetCode/35.cpp
@@ -2,34 +2,28 @@
 #include <vector>
 using namespace std;
 
-class Solution
-{
+class Solution {
 public:
-    int searchInsert(vector<int> &nums, int target)
-    {
-        int l = 0, r = nums.size() - 1;
-        int ans = nums.size();
-        while (l <= r)
-        {
-            int mid = l + (r - l) / 2;
-            if (nums[mid] >= target)
-            {
-                ans = mid;
-                r = mid - 1;
-            }
-            else
-                l = mid + 1;
-        }
-        return ans;
+  auto searchInsert(vector<int> &nums, int target) -> int {
+    int l = 0, r = static_cast<int>(nums.size()) - 1;
+    int ans = static_cast<int>(nums.size());
+    while (l <= r) {
+      int mid = l + ((r - l) >> 1);
+      if (nums[mid] >= target) {
+        ans = mid;
+        r = mid - 1;
+      } else
+        l = mid + 1;
     }
+    return ans;
+  }
 };
 
-int main()
-{
-    cin.tie(nullptr)->sync_with_stdio(false);
+auto main() -> int {
+  cin.tie(nullptr)->sync_with_stdio(false);
 
-    vector<int> nums = {1, 3, 5, 6};
-    Solution sol;
-    cout << sol.searchInsert(nums, 7) << '\n';
-    return 0;
+  vector<int> nums = {1, 3, 5, 6};
+  Solution sol;
+  cout << sol.searchInsert(nums, 7) << '\n';
+  return 0;
 }
