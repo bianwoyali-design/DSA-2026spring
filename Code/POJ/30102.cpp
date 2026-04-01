a7ec114d4e79fe324602db1f5440a4ba675db35b
bianwoyali-design <bianwoyali@gmail.com>
2026-04-01 21:37:51 +0800

    modified:   Code/POJ/30102.cpp

--- Code/POJ/30102.cpp
+++ Code/POJ/30102.cpp
@@ -1,43 +1,40 @@
-#include <algorithm>
 #include <iostream>
 #include <vector>
-
+#include <algorithm>
 
 auto main() -> int {
   std::cin.tie(nullptr)->sync_with_stdio(false);
 
-  int N;
-  std::cin >> N;
-
-  std::vector<int> h(N);
-  for (int i = 0; i < N; ++i)
-    std::cin >> h[i];
+  int n;
+  std::cin >> n;
+  std::vector<int> h(n);
+  for (auto &v : h)
+    std::cin >> v;
 
   std::vector<int> st_max;
-  std::vector<int> cand;
-  int max_len = 0;
+  std::vector<int> st_min;
 
-  for (int j = 0; j < N; ++j) {
-    while (!st_max.empty() && h[st_max.back()] < h[j]) {
+  int ans = 0;
+  for (int j = 0; j < n; ++j) {
+    while (!st_max.empty() && h[st_max.back()] < h[j])
       st_max.pop_back();
-    }
-    int lj = st_max.empty() ? -1 : st_max.back();
 
-    while (!cand.empty() && h[cand.back()] >= h[j]) {
-      cand.pop_back();
-    }
+    int left_border = st_max.empty() ? -1 : st_max.back();
 
-    if (!cand.empty()) {
-      auto it = std::upper_bound(cand.begin(), cand.end(), lj);
-      if (it != cand.end()) {
-        int best_i = *it;
-        max_len = std::max(max_len, j - best_i + 1);
+    while (!st_min.empty() && h[st_min.back()] >= h[j])
+      st_min.pop_back();
+
+    if (!st_min.empty()) {
+      auto it = std::upper_bound(st_min.begin(), st_min.end(), left_border);
+      if (it != st_min.end()) {
+        int min_l = *it;
+        ans = std::max(ans, j - min_l + 1);
       }
     }
 
-    st_max.push_back(j);
-    cand.push_back(j);
+    st_max.emplace_back(j);
+    st_min.emplace_back(j);
   }
 
-  std::cout << max_len << '\n';
+  std::cout << ans << '\n';
 }
