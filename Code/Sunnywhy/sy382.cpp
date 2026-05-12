63afb56d5d630b4f5b56eb69c408fc4c08d8d670
bianwoyali-design <bianwoyali@gmail.com>
2026-05-12 20:33:30 +0800

    Refactor topological sort implementation: separate DFS and Kahn's algorithm into distinct classes; enhance readability and maintainability
    Signed-off-by: bianwoyali-design <bianwoyali@gmail.com>

--- Code/Sunnywhy/sy382.cpp
+++ Code/Sunnywhy/sy382.cpp
@@ -1,5 +1,91 @@
+#include <algorithm>
 #include <iostream>
+#include <queue>
 #include <vector>
+
+class DFS {
+  int n = 0;
+  std::vector<int> status{};
+  std::vector<std::vector<int>> g{};
+
+public:
+  explicit DFS(const std::vector<std::vector<int>> &g) : n(g.size()), g(g) {
+    status.assign(n, 0);
+  }
+
+  auto topological_sort() -> std::vector<int> {
+    std::vector<int> res;
+
+    auto dfs = [&](auto &&self, int u) -> bool {
+      status[u] = 1;
+
+      for (auto v : g[u]) {
+        if (status[v] == 0) {
+          if (self(self, v)) {
+            return true;
+          }
+        } else if (status[v] == 1) {
+          return true;
+        }
+      }
+
+      res.emplace_back(u);
+      status[u] = 2;
+      return false;
+    };
+
+    for (int i = 0; i < n; ++i) {
+      if (!status[i]) {
+        if (dfs(dfs, i)) {
+          return {};
+        }
+      }
+    }
+
+    std::reverse(res.begin(), res.end());
+    return res;
+  }
+};
+
+class Kahn {
+  int n = 0;
+  std::vector<std::vector<int>> g{};
+
+public:
+  explicit Kahn(const std::vector<std::vector<int>> &g) : n(g.size()), g(g) {}
+
+  auto topological_sort() -> std::vector<int> {
+    std::vector<int> indegree(n, 0);
+    for (auto &node : g) {
+      for (auto v : node) {
+        ++indegree[v];
+      }
+    }
+
+    std::queue<int> q;
+    for (int i = 0; i < n; ++i) {
+      if (!indegree[i]) {
+        q.emplace(i);
+      }
+    }
+
+    std::vector<int> res;
+    while (!q.empty()) {
+      auto front = q.front();
+      q.pop();
+      res.emplace_back(front);
+
+      for (auto v : g[front]) {
+        --indegree[v];
+        if (!indegree[v]) {
+          q.emplace(v);
+        }
+      }
+    }
+
+    return res.size() == n ? res : std::vector<int>{};
+  }
+};
 
 auto main() -> int {
   std::cin.tie(nullptr)->sync_with_stdio(false);
@@ -14,38 +100,7 @@ auto main() -> int {
     g[u].emplace_back(v);
   }
 
-  std::vector<bool> visited(n, false);
-  std::vector<bool> stack(n, false);
-
-  auto dfs = [&](auto &&self, int node) -> bool {
-    visited[node] = true;
-    stack[node] = true;
-
-    for (auto &&v : g[node]) {
-      if (!visited[v] && self(self, v)) {
-        return true;
-      } else if (stack[v]) {
-        return true;
-      }
-    }
-
-    stack[node] = false;
-    return false;
-  };
-
-  bool flag = false;
-  for (int i = 0; i < n; ++i) {
-    if (!visited[i]) {
-      flag = dfs(dfs, i);
-      if (flag) {
-        break;
-      }
-    }
-  }
-
-  if (flag) {
-    std::cout << "Yes\n";
-  } else {
-    std::cout << "No\n";
-  }
+  Kahn kahn_sol(g);
+  DFS dfs_sol(g);
+  std::cout << (kahn_sol.topological_sort() == dfs_sol.topological_sort()) << '\n';
 }
