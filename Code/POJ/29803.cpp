a6494985d99d060bffa5e2e5e3119452521bab6f
bianwoyali-design <bianwoyali@gmail.com>
2026-05-26 23:01:34 +0800

    Refactor POJ 29803 solution: update variable types and improve priority queue handling
    Signed-off-by: bianwoyali-design <bianwoyali@gmail.com>

--- Code/POJ/29803.cpp
+++ Code/POJ/29803.cpp
@@ -1,3 +1,4 @@
+#include <array>
 #include <functional>
 #include <iostream>
 #include <queue>
@@ -7,27 +8,27 @@
 auto main() -> int {
   std::cin.tie(nullptr)->sync_with_stdio(false);
 
-  int n, m, T;
+  int n, m;
+  long long T;
   std::cin >> n >> m >> T;
-  std::vector adj(n, std::vector<std::tuple<int, int, int>>());
-  int l = 100, r = 0;
+  std::vector adj(n, std::vector<std::tuple<int, long long, int>>());
   for (int i = 0; i < m; ++i) {
-    int u, v, t, a;
+    int u, v, a;
+    long long t;
     std::cin >> u >> v >> t >> a;
     --u, --v;
     adj[u].emplace_back(v, t, a);
     adj[v].emplace_back(u, t, a);
-    l = std::min(l, a);
-    r = std::max(r, a);
   }
 
   auto check = [&](int x) -> bool {
-    constexpr static int INF = 0x3f3f3f3f;
-    std::vector dist(n, std::vector<int>(2, INF));
+    constexpr static long long INF = 4e18;
+    std::vector dist(n, std::array<long long, 2>{INF, INF});
     dist[0][0] = 0;
 
-    std::priority_queue<std::tuple<int, int, int>,
-                        std::vector<std::tuple<int, int, int>>, std::greater<>>
+    std::priority_queue<std::tuple<long long, int, int>,
+                        std::vector<std::tuple<long long, int, int>>,
+                        std::greater<>>
         pq;
     pq.emplace(0, 0, 0);
 
@@ -35,8 +36,14 @@ auto main() -> int {
       auto [t, u, status] = pq.top();
       pq.pop();
 
-      if (t > dist[u][status]) {
+      if (t != dist[u][status]) {
         continue;
+      }
+      if (t > T) {
+        continue;
+      }
+      if (u == n - 1) {
+        return true;
       }
 
       for (auto [v, weight, damage] : adj[u]) {
@@ -57,6 +64,8 @@ auto main() -> int {
     return std::min(dist[n - 1][0], dist[n - 1][1]) <= T;
   };
 
+  int l = 0;
+  int r = 100;
   while (l < r) {
     int mid = l + ((r - l) >> 1);
     if (check(mid)) {
